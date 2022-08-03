4b202960da60bc5cd213f1f21924e38c3d375d5a
Vanza Setia <venusbumi2@gmail.com>
2022-08-03 18:08:44 +0700

    Make `root` variable as global variable

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
 // @ts-nocheck
 (function () {
-  'use strict';
+  "use strict";
   const themeSwitcherCheckbox = document.querySelector(
-    '.js-theme-switcher-checkbox'
+    ".js-theme-switcher-checkbox"
   );
-  const themeSwitcherLabel = document.querySelector('.js-theme-switcher-label');
-  const themeSwitcherIcon = document.querySelector('.js-theme-switcher-icon');
+  const themeSwitcherLabel = document.querySelector(".js-theme-switcher-label");
+  const themeSwitcherIcon = document.querySelector(".js-theme-switcher-icon");
+  const root = document.querySelector("html");
 
   const setThemeSwitcherStateForDarkMode = () => {
-    themeSwitcherCheckbox.setAttribute('checked', '');
-    themeSwitcherLabel.textContent = 'light';
-    themeSwitcherIcon.setAttribute('href', '/svg/sprite.svg#sun');
+    themeSwitcherCheckbox.setAttribute("checked", "");
+    themeSwitcherLabel.textContent = "light";
+    themeSwitcherIcon.setAttribute("href", "/svg/sprite.svg#sun");
   };
 
   const setThemeSwitcherStateForLightMode = () => {
-    themeSwitcherCheckbox.removeAttribute('checked');
-    themeSwitcherLabel.textContent = 'dark';
-    themeSwitcherIcon.setAttribute('href', '/svg/sprite.svg#moon');
+    themeSwitcherCheckbox.removeAttribute("checked");
+    themeSwitcherLabel.textContent = "dark";
+    themeSwitcherIcon.setAttribute("href", "/svg/sprite.svg#moon");
   };
 
   const changeTheme = () => {
-    const root = document.querySelector('html');
     const isSystemThemeDark = matchMedia(
-      '(prefers-color-scheme: dark)'
+      "(prefers-color-scheme: dark)"
     ).matches;
 
     if (isSystemThemeDark) {
-      root.classList.toggle('is-light');
-      if (root.classList.contains('is-light')) {
+      root.classList.toggle("is-light");
+      if (root.classList.contains("is-light")) {
         setThemeSwitcherStateForLightMode();
-        localStorage.setItem('root-class', 'is-light');
+        localStorage.setItem("root-class", "is-light");
       } else {
         setThemeSwitcherStateForDarkMode();
         localStorage.clear();
       }
     } else {
-      root.classList.toggle('is-dark');
-      if (root.classList.contains('is-dark')) {
+      root.classList.toggle("is-dark");
+      if (root.classList.contains("is-dark")) {
         setThemeSwitcherStateForDarkMode();
-        localStorage.setItem('root-class', 'is-dark');
+        localStorage.setItem("root-class", "is-dark");
       } else {
         setThemeSwitcherStateForLightMode();
         localStorage.clear();
@@ -48,12 +48,12 @@
 
   const setThemeSwitcherState = () => {
     const isSystemThemeDark = matchMedia(
-      '(prefers-color-scheme: dark)'
+      "(prefers-color-scheme: dark)"
     ).matches;
-    const rootClassList = document.querySelector('html').classList[0];
+    const rootClassList = document.querySelector("html").classList[0];
 
     if (rootClassList) {
-      if (rootClassList === 'is-dark') {
+      if (rootClassList === "is-dark") {
         setThemeSwitcherStateForDarkMode();
       } else {
         setThemeSwitcherStateForLightMode();
@@ -67,14 +67,13 @@
     }
   };
 
-  themeSwitcherCheckbox.addEventListener('change', changeTheme);
-  document.addEventListener('DOMContentLoaded', setThemeSwitcherState);
+  themeSwitcherCheckbox.addEventListener("change", changeTheme);
+  document.addEventListener("DOMContentLoaded", setThemeSwitcherState);
   window
-    .matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', ({ matches }) => {
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", ({ matches }) => {
       const isSystemThemeDark = matches;
-      const root = document.querySelector('html');
-      root.classList.remove('is-light', 'is-dark');
+      root.classList.remove("is-light", "is-dark");
       localStorage.clear();
 
       if (isSystemThemeDark) {
