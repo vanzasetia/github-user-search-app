a2bfe5aa4fad8a6bc6d9feac9c067c866b48aae2
Vanza Setia <venusbumi2@gmail.com>
2022-08-08 18:36:33 +0700

    Initial responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 (function () {
   "use strict";
 
--- js/user-preference.js
+++ js/user-preference.js
@@ -1,8 +1,6 @@
-// @ts-nocheck
 (function () {
   "use strict";
   const rootClassTheme = localStorage.getItem("root-class");
-
   if (rootClassTheme) {
     const root = document.querySelector("html");
     root.classList.add(rootClassTheme);
